1fdea36d452b64277f2845b74c7ff656a45c8503
mAtthEwwww <mattwaizuen@outlook.com>
2017-06-07 00:35:07 +0800

    modified

--- tex/compile.bat
+++
@@ -1,7 +0,0 @@
-xelatex -shell-escape PatternRecognition.tex
-
-xelatex -shell-escape PatternRecognition.tex
-
-del *.aux *.log *.bbl *.blg
-
-PatternRecognition.pdf
--- tex/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-xelatex -shell-escape PatternRecognition.tex
-bibtex PatternRecognition.aux
-xelatex -shell-escape PatternRecognition.tex
-xelatex -shell-escape PatternRecognition.tex
-rm *.aux *.log
-evince PatternRecognition.pdf
